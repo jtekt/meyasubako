b3cf99ec49d2d5f7f0bdca24b2f88250e9de0e79
Maxime Moreillon <m.moreillon@gmail.com>
2020-06-30 05:17:45 +0000

    full restfulification

---
+++ controllers/complaints.js
@@ -0,0 +1,89 @@
+const mongodb = require('mongodb')
+
+const MongoClient = mongodb.MongoClient
+const ObjectID = mongodb.ObjectID
+
+const db_name = 'mendokusai'
+const monku_collection_name = 'monku'
+
+const mongodb_options = {
+  useUnifiedTopology: true,
+}
+
+
+exports.create_complaint = (req, res) => {
+  if(!('content' in req.body)) return res.status(400).send('Content not present in request body')
+
+  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
+    if (err) return res.status(500).send('Error connecting to DB')
+
+    let monku = {
+      content: req.body.content,
+      likes: 0,
+      proposals: [],
+    }
+
+    db.db(db_name)
+    .collection(monku_collection_name)
+    .insertOne(monku, (err, result) => {
+      if (err) return res.status(500).send('Error querying DB')
+      db.close()
+      res.send(result)
+    })
+  })
+}
+
+exports.get_complaint = (req, res) => {
+
+  // Get id from query parameters
+  let monku_id = req.params.monku_id
+  let query = {}
+  if(monku_id) query._id = ObjectID(monku_id)
+
+  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
+    if (err) return res.status(500).send('Error connecting to DB')
+    db.db(db_name)
+    .collection(monku_collection_name)
+    .aggregate([
+      { $match: query },
+      { $lookup: {
+         from: 'proposals',
+         localField: 'proposals',
+         foreignField: '_id',
+         as: 'proposals'
+       }
+     },
+    ])
+    .toArray((err, result) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).send('Error querying DB')
+      }
+      db.close()
+      res.send(result)
+    })
+  })
+}
+
+exports.delete_complaint = (req, res) => {
+
+  // Get id from query parameters
+  let monku_id = req.params.monku_id
+  if(!monku_id) return res.status(400).send('ID undefined')
+
+  let query = {_id : ObjectID(monku_id)}
+
+  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
+    if (err) return res.status(500).send('Error connecting to DB')
+    db.db(db_name)
+    .collection(monku_collection_name)
+    .deleteOne(query, (err, result) => {
+      if (err) {
+        console.log(err)
+        return res.status(500).send('Error querying DB')
+      }
+      db.close()
+      res.send(result)
+    })
+  })
+}
--- main.js
+++ main.js
@@ -17,153 +17,52 @@ const mongodb_options = {
   useUnifiedTopology: true,
 }
 
+
+const complaints_controller = require('./controllers/complaints.js')
+const proposals_controller = require('./controllers/proposals.js')
+const votes_controller = require('./controllers/votes.js')
+
+const app_port = process.env.APP_PORT || 80
+
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-var app_port = 80
-if(process.env.APP_PORT) app_port = process.env.APP_PORT
-
 app.get('/', (req,res) => {
-  res.send(`Monku API, Maxime MOREILLON <br> MonboDB URL: ${process.env.MONGODB_URL}`)
-})
-app.post('/monku', (req, res) => {
-  if(!('content' in req.body)) return res.status(400).send('Content not present in request body')
-
-  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
-    if (err) return res.status(500).send('Error connecting to DB')
-
-    let monku = {
-      content: req.body.content,
-      likes: 0,
-      proposals: [],
-    }
-
-    db.db(db_name)
-    .collection(monku_collection_name)
-    .insertOne(monku, (err, result) => {
-      if (err) return res.status(500).send('Error querying DB')
-      db.close()
-      res.send(result)
-    })
-  })
+  res.send(`
+    Monku API, Maxime MOREILLON<br>
+    MonboDB URL: ${process.env.MONGODB_URL}
+    `)
 })
 
-app.get('/monku', (req, res) => {
+// Complaints
+app.route('/monku')
+  .post(complaints_controller.create_complaint)
+  .get(complaints_controller.get_complaint)
 
-  // Get id from query parameters
-  let query = {}
-  if(('_id' in req.query)) query._id = ObjectID(req.query._id)
+app.route('/monku/:monku_id')
+  .get(complaints_controller.get_complaint)
+  .delete(complaints_controller.delete_complaint)
 
-  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
-    if (err) return res.status(500).send('Error connecting to DB')
-    db.db(db_name)
-    .collection(monku_collection_name)
-    .aggregate([
-      { $match: query },
-      { $lookup: {
-         from: 'proposals',
-         localField: 'proposals',
-         foreignField: '_id',
-         as: 'proposals'
-       }
-     },
-    ])
-    .toArray((err, result) => {
-      if (err) {
-        console.log(err)
-        return res.status(500).send('Error querying DB')
-      }
-      db.close()
-      res.send(result)
-    })
-  })
-})
+app.route('/monku/:monku_id/vote')
+  .post(votes_controller.vote)
 
+// Proposals
+app.route('/monku/:monku_id/proposals')
+  .post(proposals_controller.create_proposal)
+  .get(proposals_controller.get_proposals)
 
+app.route('/monku/:monku_id/proposals/:proposal_id')
+  .delete(proposals_controller.delete_proposal)
 
-app.post('/proposal', (req, res) => {
-  if(!('monku_id' in req.body)) return res.status(400).send('monku_id not present in request body')
-  if(!('content' in req.body)) return res.status(400).send('Content not present in request body')
+app.route('/monku/:monku_id/proposals/:proposal_id/vote')
+  .post(votes_controller.vote)
 
-  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
-    if (err) return res.status(500).send('Error connecting to DB')
+// Simplified routes to interact directly with proposals
+app.route('/proposals/:proposal_id')
+  .delete(proposals_controller.delete_proposal)
 
-    let proposal = {
-      content: req.body.content,
-      monku_id: req.body.monku_id,
-      likes: 0,
-    }
-
-    db.db(db_name).collection(proposals_collection_name)
-    .insertOne(proposal, (err, result) => {
-      if (err) return res.status(500).send('Error querying DB')
-
-      let filter = {_id: ObjectID(req.body.monku_id)}
-      let update = {$push: {proposals: ObjectID(result.insertedId)}} // Not sure if ObjectID is needed here
-
-      db.db(db_name).collection(monku_collection_name)
-      .updateOne(filter,update, (err, result) => {
-        if (err) return res.status(500).send('Error querying DB')
-        db.close()
-        res.send(result)
-
-      })
-
-    })
-  })
-})
-
-app.get('/proposal', (req, res) => {
-
-  // Get id from query parameters
-  if(!('monku_id' in req.query)) return res.status(400).send('monku_id not present in request body')
-
-  let query = {monku_id: req.query.monku_id}
-  if(('_id' in req.query)) query._id = ObjectID(req.query._id)
-
-  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
-    if (err) return res.status(500).send('Error connecting to DB')
-    db.db(db_name)
-    .collection(proposals_collection_name)
-    .find(query)
-    .toArray((err, result) => {
-      if (err) return res.status(500).send('Error querying DB')
-      db.close()
-      res.send(result)
-    })
-  })
-})
-
-app.post('/vote', (req, res) => {
-
-  if(!('_id' in req.body)) return res.status(400).send('ID not present in request body')
-  if(!('vote' in req.body)) return res.status(400).send('Vote not present in request body')
-  if(!('collection' in req.body)) return res.status(400).send('Collection not present in request body')
-
-  MongoClient.connect(process.env.MONGODB_URL,mongodb_options, (err, db) => {
-    if (err) return res.status(500).send('Error connecting to DB')
-
-    let filter = {
-      _id: ObjectID(req.body._id)
-    }
-
-    let update = {
-      $inc: {
-        likes: req.body.vote
-      }
-    }
-
-    let options = {returnOriginal:false}
-
-
-    db.db(db_name).collection(req.body.collection)
-    .findOneAndUpdate(filter, update, options, (err, result) => {
-      if (err) return res.status(500).send('Error querying DB')
-      db.close()
-      res.send(result)
-    })
-  })
-})
+app.route('/proposals/:proposal_id/vote')
+  .post(votes_controller.vote)
 
 app.listen(app_port, () => console.log(`Mendokusai running on port ${app_port}`))
