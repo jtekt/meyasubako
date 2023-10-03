64dece462f16706bd60ba81d0562237fe167d38d
Maxime Moreillon <m.moreillon@gmail.com>
2023-10-03 15:38:21 +0900

    cors, port

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia"
+import { cors } from "@elysiajs/cors"
 import { createItem, readItems, readItem, vote } from "./controllers/items"
+
+const { ELYSIA_PORT = 80 } = process.env
+
 const app = new Elysia()
-// TODO: cors
+app.use(cors())
 
 app.post("/items", createItem)
 app.get("/items", readItems)
@@ -12,6 +16,6 @@ app.post("/items/:id/like", vote(1))
 app.post("/items/:id/dislikes", vote(-1))
 app.post("/items/:id/dislike", vote(-1))
 
-app.listen(3001, () => {
+app.listen(ELYSIA_PORT, () => {
   console.log(`Elysia listening on port ${app.server?.port}`)
 })
