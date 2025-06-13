86003ef564a4a5e25596c95ba8b0247c8b7fa5ce
Maxime Moreillon <m.moreillon@gmail.com>
2025-06-13 13:54:23 +0900

    tailind, daisyui, db connection

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: {
-    ssr: { external: ["@prisma/client"] }
-  }
+    ssr: { external: ["@prisma/client"] },
+    plugins: [tailwindcss()],
+  },
 });
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,17 @@
 import { createAsync, type RouteDefinition } from "@solidjs/router";
-import { getUser, logout } from "~/lib";
+import { getItems } from "~/lib";
 
 export const route = {
   preload() {
-    getUser();
-  }
+    getItems();
+  },
 } satisfies RouteDefinition;
 
 export default function Home() {
-  const user = createAsync(() => getUser(), { deferStream: true });
+  const items = createAsync(() => getItems(), { deferStream: true });
   return (
-    <main class="w-full p-4 space-y-2">
-      <h2 class="font-bold text-3xl">Hello {user()?.username}</h2>
-      <h3 class="font-bold text-xl">Message board</h3>
-      <form action={logout} method="post">
-        <button name="logout" type="submit">
-          Logout
-        </button>
-      </form>
+    <main>
+      <p> {JSON.stringify(items())}</p>
     </main>
   );
 }
