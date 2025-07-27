7c5a0d6527dada74c181792ba33f42249829b904
Maxime Moreillon <m.moreillon@gmail.com>
2025-07-28 08:00:35 +0900

    fixing cicd build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
-import { PrismaClient } from "../generated/prisma";
+import { PrismaClient } from "~/../generated/prisma/client";
 
-const prisma = new PrismaClient(); //.$extends(withAccelerate());
+const prisma = new PrismaClient();
 
 export default prisma;
